3199a580e33742039e5bb3fd99fe5494c80a6eda
Himaja Vadaga <himaja.vadaga@gmail.com>
2016-11-01 02:12:07 -0400

    2_GET_Method
    Get_all and Get_by_User

--- gs-spring-boot-initial-1/src/main/java/hello/HelloController.java
+++ gs-spring-boot-initial-1/src/main/java/hello/HelloController.java
@@ -6,7 +6,11 @@ import redis.clients.jedis.Jedis;
 
 import java.text.ParseException;
 import java.util.HashMap;
+import java.util.Map;
 
+import javax.ws.rs.Produces;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.http.HttpHeaders;
@@ -29,30 +33,37 @@ public class HelloController {
 	public String create(@RequestHeader HttpHeaders headers, @RequestBody String entity)
 			throws Exception {
 
+		HashMap object= new HashMap ();
 		JSONParser parser = new JSONParser();
-		// parser to parse through postman
 		Jedis jedis = new Jedis("127.0.0.1");
-		// jedis object creation
-		JSONObject jsonobject = (JSONObject) parser.parse(entity);
-		// jsonobject to parse through jedis entity
-		jedis.set("Test123", jsonobject.toJSONString());
-		// setting jedis object and convert to string for displaying it on
-		// postman
+		object = (HashMap) parser.parse(entity);
+		jedis.set("Test123", object.toString());
 		System.out.println(jedis.get("Test123"));
-
-		/*
-		 * HashMap object= new HashMap (); Jedis jedis = new Jedis("127.0.0.1");
-		 * 
-		 * jedis.set("Test123", entity);
-		 * 
-		 * String s = jedis.get("Test123"); System.out.println(s); JSONParser
-		 * parser = new JSONParser(); object =(HashMap) parser.parse( entity);
-		 */
-
-		return jsonobject.toJSONString();
-		// Postman displays the result
+		return object.toString();
 	}
-
-	// crud operations
+	//display all data
+	@RequestMapping(value = "/plan", method= RequestMethod.GET)	
+	public String display(@RequestHeader HttpHeaders headers )
+		throws Exception{
+		
+		JSONParser parser = new JSONParser();
+		Jedis jedis = new Jedis("127.0.0.1");
+		JSONObject jsonobject = (JSONObject) parser.parse();
+		jedis.set("Test123", jsonobject.toJSONString());
+		System.out.println(jedis.get("Test123"));
+		return jsonobject.toJSONString();
+	}
+	//display all data of a user
+	@RequestMapping(value = "/plan/{id}", method= RequestMethod.GET)	
+	public String displayuser(@RequestHeader HttpHeaders headers )
+		throws Exception{
+		
+		JSONParser parser = new JSONParser();
+		Jedis jedis = new Jedis("127.0.0.1");
+		JSONObject jsonobject = (JSONObject) parser.parse();
+		jedis.set("Test123", jsonobject.toJSONString());
+		System.out.println(jedis.get("Test123"));
+		return jsonobject.toJSONString();
+	}
 
 }
